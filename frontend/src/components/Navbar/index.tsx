c1bb3fc557ae8580f701a6d407d4769b370a90a4
Tito0ficial <titol.oficial@gmail.com>
2025-10-09 11:16:46 -0300

    Home, navigation

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,12 +1,13 @@
+import { NavLink } from 'react-router-dom';
 import imgDsDark from '../../assets/img/ds-dark.svg';
 
 export default function Navbar() {
     return (
         <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-light border-bottom shadow-sm">
             <div className="container">
-                <nav className="my-2 my-md-0 mr-md-3">
+                <NavLink to="/" className="my-2 my-md-0 mr-md-3">
                     <img src={imgDsDark} alt="DevSuperior" width="120" />
-                </nav>
+                </NavLink>
             </div>
         </div>
     );
--- frontend/src/routes/Home/index.tsx
+++ frontend/src/routes/Home/index.tsx
@@ -1,7 +1,23 @@
+import { Link } from "react-router-dom";
+import Footer from "../../components/Footer";
+import Navbar from "../../components/Navbar";
+
 export default function Home() {
     return (
-        <div>
-            <h1>Página home</h1>
-        </div>
+        <>
+            <Navbar/>
+            <div className="container">
+                <div className="jumbotron">
+                    <h1 className="display-4 mt-5">DSVendas</h1>
+                    <p className="lead">Analise o desempenho das suas vendas por diferentes perspectivas</p>
+                    <hr/>
+                    <p>Esta aplicação consiste em exibir um dashboard a partir de dados fornecidos por um back end construído com Spring Boot.</p>
+                    <Link className="btn btn-primary btn-lg" to="/dashboard">
+                        Acessar dashboard
+                    </Link>
+                </div>
+            </div>
+            <Footer/>
+        </>
     );
 }
