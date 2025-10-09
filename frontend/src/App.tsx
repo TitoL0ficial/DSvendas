4eab5f1eb2d0eef4aa2625a5158ec5c1b4812596
Tito0ficial <titol.oficial@gmail.com>
2025-10-09 11:04:48 -0300

    Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,36 +1,16 @@
-import DonutChart from "./components/DonutChart";
-import BarChart from "./components/BarChart";
-import DataTable from "./components/DataTable";
-import Footer from "./components/Footer";
-import Navbar from "./components/Navbar";
+import Dashboard from "./routes/Dashboard";
+import Home from "./routes/Home";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
 
   return (
-    <>
-      <Navbar/>
-      <div className="container">
-        <h1 className="text-primary py-3">Dashboard de Vendas</h1>
-
-        <div className="row px-3">
-          <div className="col-sm 6">
-            <h5 className="text-center text-secondary">Taxa de sucesso (%)</h5>
-            <BarChart/>
-          </div>
-          <div className="col-sm 6">
-            <h5 className="text-center text-secondary">Todas vendas</h5>
-            <DonutChart/>
-          </div>
-        </div>
-
-        <div className="py-3">
-          <h2 className="text-primary">Todas vendas</h2>
-        </div>
-
-        <DataTable/>
-      </div>
-      <Footer/>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="dashboard" element={<Dashboard/>} />        
+      </Routes>
+    </BrowserRouter>
   );
 }
 
